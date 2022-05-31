3d3e35d950d5bd5ed6ffb2fdba337bc5c35dc9c1
Diogo Lemos <diogo.lemos@blip.pt>
2022-05-31 11:56:11 +0100

    Check for sonar host url

--- script/check-quality-gate.sh
+++ script/check-quality-gate.sh
@@ -14,8 +14,13 @@ if [[ ! -f "$metadataFile" ]]; then
    exit 1
 fi
 
-serverUrl="$(sed -n 's/serverUrl=\(.*\)/\1/p' "${metadataFile}")"
-ceTaskUrl="$(sed -n 's/ceTaskUrl=\(.*\)/\1/p' "${metadataFile}")"
+if [[ -z "${SONAR_HOST_URL}" ]]; then
+   serverUrl="${SONAR_HOST_URL}"
+   ceTaskUrl="${SONAR_HOST_URL} + $(sed -n 's/.*api//p' "${metadataFile}")"
+else
+   serverUrl="$(sed -n 's/serverUrl=\(.*\)/\1/p' "${metadataFile}")"
+   ceTaskUrl="$(sed -n 's/ceTaskUrl=\(.*\)/\1/p' "${metadataFile}")"
+fi
 
 if [ -z "${serverUrl}" ] || [ -z "${ceTaskUrl}" ]; then
   echo "Invalid report metadata file."
