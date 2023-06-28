c32e6c57093bd6ff51fbbc8f1fd0731b052e5d11
Conner Delahanty <cdelahanty@craniusmed.com>
2023-06-28 13:02:52 -0400

    change sleep 5s to sleep 5 to support MacOS runners

--- script/check-quality-gate.sh
+++ script/check-quality-gate.sh
@@ -39,7 +39,7 @@ status="$(jq -r '.task.status' <<< "$task")"
 
 until [[ ${status} != "PENDING" && ${status} != "IN_PROGRESS" ]]; do
     printf '.'
-    sleep 5s
+    sleep 5
     task="$(curl --location --location-trusted --max-redirs 10 --silent --fail --show-error --user "${SONAR_TOKEN}": "${ceTaskUrl}")"
     status="$(jq -r '.task.status' <<< "$task")"
 done
